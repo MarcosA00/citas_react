8079ef7b24b696e1edd5029edd59bc6294d0f2f6
Marcos Aarón <maracastror@gmail.com>
2023-09-11 11:55:45 -0600

    mod: Se modifica el header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return(
     <div>
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-          Seguimiento de Pacientes { ' ' }
+          Seguimiento de Citas { ' ' }
           <span className="text-indigo-600">
             Veterinaria
           </span>
